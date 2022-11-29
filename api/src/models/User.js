3ba14c787566d9d4cfccc0daf3a41aa08a9663d9
renauzca <renauzca2103@gmail.com>
2022-11-29 12:19:37 -0300

    primero push a mi rama

--- api/src/models/User.js
+++ api/src/models/User.js
@@ -15,7 +15,23 @@ module.exports = (sequelize) => {
     contrasena: {
       type: DataTypes.STRING,
       allowNull: false,
+    },
+    nombreUsuario: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    imagenPerfil: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      defaultValue: 
+      "https://th.bing.com/th?q=User+Jpg&w=120&h=120&c=1&rs=1&qlt=90&cb=1&pid=InlineBlock&mkt=es-AR&cc=AR&setlang=es&adlt=moderate&t=1&mw=247"
+    },
+    rol:{},
+    numeroCelular:{
+      type: DataTypes.INTERGER,
+      allowNull: true
     }
+
 
   });
 };
